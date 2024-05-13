e4fbdd0fcb07c6fcdd3cf600978dbf4b567889fb
Vickouma77 <Vickprestone20@gmail.com>
2024-05-13 11:30:41 +0300

    a placeholder for the explore page

---
+++ src/pages/ExplorePage.js
@@ -0,0 +1,12 @@
+import Body from "../components/Body"
+
+function ExplorePage() {
+  return (
+    <Body sidebar>
+        <h1>Explore</h1>
+        <p>TODO</p>
+    </Body>
+  )
+}
+
+export default ExplorePage
