5b33bf0acc0ec2970dd577a7881630cd3e11425b
Vickouma77 <Vickprestone20@gmail.com>
2024-05-14 15:43:57 +0300

    user page

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter, Route, Routes, Navigate } from 'react-router-dom'
 import Header from './components/Header'
 import FeedPage from './pages/FeedPage';
 import ExplorePage from './pages/ExplorePage';
+import UserPage from './pages/UserPage';
 import LoginPage from './pages/LoginPage'
 
 function App() {
@@ -15,6 +16,7 @@ function App() {
         <Routes>
             <Route path="/" element={<FeedPage />} />
             <Route path="/explore" element={<ExplorePage />} />
+            <Route path="/user/:username" element={<UserPage />} />
             <Route path="/login" element={<LoginPage />} />
             <Route path="*" element={<Navigate to="/" />} />
         </Routes>
