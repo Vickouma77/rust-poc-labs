4ad4f32c5e1f1cd0af3ebcad37a4adaa6b84993e
Vickouma77 <Vickprestone20@gmail.com>
2024-05-14 15:43:33 +0300

    user page

---
+++ src/pages/UserPage.js
@@ -0,0 +1,16 @@
+import { useParams } from "react-router-dom";
+import Body from '../components/Body';
+
+function UserPage() {
+    const { username } = useParams();
+
+
+  return (
+    <Body sidebar>
+      <h1>{username}</h1>
+      <p>TODO</p>
+    </Body>
+  );
+}
+
+export default UserPage
